970c2d5f987e394e88ef592f3576001741b0ede7
Fredrik <you@example.com>
2019-01-31 17:02:26 +0100

    .

--- App.js
+++ App.js
@@ -1,17 +1,30 @@
 import React, { Component, Fragment } from 'react';
 import { StyleSheet, Text, View, Image } from 'react-native';
+import { random } from 'lodash';
 
 export default class App extends Component {
+  randomColor() {
+    return `rgb(${random(0,255)},${random(0,255)},${random(0,255)})`;
+  }
+
+  renderNavbarItem(text: string) {
+    return (
+      <View style={{ padding: 10, backgroundColor: this.randomColor() }}>
+        <Text style={{fontSize: 40}}>{text}</Text>
+      </View>
+    );
+  } 
+
   render() {
     return (
       <View style={{ flex: 1, backgroundColor: 'rgb(255, 255, 100)' }}>
-        <View style={{ flexDirection: 'row', backgroundColor: 'lightblue' }}>
-          <Text style={{fontSize: 40}}>1</Text>
-          <Text style={{fontSize: 40}}>2</Text>
-          <Text style={{fontSize: 40}}>3</Text>
+        <View style={{ flexDirection: 'row', backgroundColor: 'lightblue', borderBottomColor: 'black', borderBottomWidth: 1 }}>
+          {this.renderNavbarItem('ett')}
+          {this.renderNavbarItem('två')}
+          {this.renderNavbarItem('tre')}
         </View>
-        <View>
-          <Text style={{fontSize: 40}}>1</Text>
+        <View style={{ padding: 10 }}>
+          <Text style={{fontSize: 40}}>Test</Text>
         </View>
       </View>
     );
