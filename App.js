4ded923a89bac6a977d8c66c291dccd794e996fe
Fredrik <you@example.com>
2019-01-31 16:00:31 +0100

    playing around

--- App.js
+++ App.js
@@ -1,27 +1,20 @@
 import React, { Component, Fragment } from 'react';
 import { StyleSheet, Text, View, Image } from 'react-native';
 
-import { Layout } from './layouts';
-
 export default class App extends Component {
   render() {
-    let pic = {
-      uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
-    };
     return (
-      <Layout>
-        <View style={styles.container}>
-          <Text>{'Open up App.js to start\n working on your app!'}</Text>
-          <Image source={pic} style={{width: 193, height: 110}}/>
+      <View style={{ flex: 1, backgroundColor: 'rgb(255, 255, 100)' }}>
+        <View style={{ flexDirection: 'row', backgroundColor: 'lightblue' }}>
+          <Text style={{fontSize: 40}}>1</Text>
+          <Text style={{fontSize: 40}}>2</Text>
+          <Text style={{fontSize: 40}}>3</Text>
         </View>
-      </Layout>
+        <View>
+          <Text style={{fontSize: 40}}>1</Text>
+        </View>
+      </View>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-  },
-});
